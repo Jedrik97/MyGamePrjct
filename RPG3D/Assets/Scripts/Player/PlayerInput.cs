61d6632698caeeb68b9a7b6a7dbe3c29d66408bf
Jedrik97 <156681308+Jedrik97@users.noreply.github.com>
2025-02-27 08:32:12 +0300

    update

--- RPG3D/Assets/Scripts/Player/PlayerInput.cs
+++ RPG3D/Assets/Scripts/Player/PlayerInput.cs
@@ -3,15 +3,17 @@ using System;
 
 public class PlayerInput : MonoBehaviour
 {
-    public event Action<Vector2> OnMove;
+    public event Action<Vector2, bool> OnMove; // bool для бега
     public event Action OnJump;
 
     private void Update()
     {
         Vector2 movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        bool isRunning = Input.GetKey(KeyCode.LeftShift);
+
         if (movement.magnitude > 0.1f)
         {
-            OnMove?.Invoke(movement);
+            OnMove?.Invoke(movement, isRunning);
         }
 
         if (Input.GetButtonDown("Jump"))
--- RPG3D/Assets/Scripts/Player/PlayerMovement.cs
+++ RPG3D/Assets/Scripts/Player/PlayerMovement.cs
@@ -7,99 +7,100 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float walkSpeed = 3.5f;
     [SerializeField] private float runSpeed = 7f;
     [SerializeField] private float rotationSpeed = 200f;
-    [SerializeField] private float jumpForce = 5f;
+    [SerializeField] private float jumpForce = 1f;
     [SerializeField] private float gravity = -9.81f;
 
     private CharacterController _controller;
     private Vector3 _velocity;
     private bool _isGrounded;
-
-    // Ссылка на камеру для движения относительно её направления
     private Transform cameraTransform;
-
-    
+    private Animator animator;
 
     private void Awake()
     {
         _controller = GetComponent<CharacterController>();
-        
+        animator = GetComponent<Animator>();
     }
+
     private void Start()
     {
         cameraTransform = Camera.main.transform;
+        PlayerInput input = GetComponent<PlayerInput>();
+        input.OnMove += HandleMovement;
+        input.OnJump += HandleJump;
     }
 
     private void Update()
     {
         ApplyGravity();
-        HandleMovement();
-        HandleJump();
     }
 
-    private void HandleMovement()
+    private void HandleMovement(Vector2 input, bool isRunning)
     {
-        float moveVertical = Input.GetAxis("Vertical");   // W/S
-        float moveHorizontal = Input.GetAxis("Horizontal"); // A/D
-        bool isRunning = Input.GetKey(KeyCode.LeftShift);
         float currentSpeed = isRunning ? runSpeed : walkSpeed;
+        bool isMovingBackward = input.y < 0;
 
-        // Если ПКМ зажата – движение относительно камеры
-        if (Input.GetMouseButton(1))
+        if (input.magnitude > 0.1f)
         {
-            Vector3 inputDirection = new Vector3(moveHorizontal, 0f, moveVertical);
-            if (inputDirection.sqrMagnitude > 0.01f)
+            Vector3 moveDirection = new Vector3(input.x, 0, input.y).normalized;
+            Vector3 worldMove = cameraTransform.forward * moveDirection.z + cameraTransform.right * moveDirection.x;
+            worldMove.y = 0;
+            worldMove.Normalize();
+            _controller.Move(worldMove * currentSpeed * Time.deltaTime);
+
+            if (!isMovingBackward)
             {
-                inputDirection.Normalize();
-
-                // Направления относительно камеры (без компонента Y)
-                Vector3 camForward = cameraTransform.forward;
-                camForward.y = 0;
-                camForward.Normalize();
-
-                Vector3 camRight = cameraTransform.right;
-                camRight.y = 0;
-                camRight.Normalize();
-
-                // Вычисляем направление движения
-                Vector3 moveDirection = (camForward * inputDirection.z) + (camRight * inputDirection.x);
-                moveDirection.Normalize();
-
-                _controller.Move(moveDirection * currentSpeed * Time.deltaTime);
-
-                // Плавно поворачиваем персонажа в направлении движения
-                Quaternion targetRotation = Quaternion.LookRotation(moveDirection);
-                transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+                Quaternion targetRotation = Quaternion.LookRotation(worldMove);
+                transform.rotation =
+                    Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
             }
+
+            // Передаём параметры в анимацию
+            animator.SetFloat("MoveSpeed", currentSpeed);
+            animator.SetBool("IsMovingBackward", isMovingBackward);
         }
-        else // Если ПКМ не зажата – стандартное поведение
+        else
         {
-            if (Mathf.Abs(moveVertical) > 0.01f)
-            {
-                Vector3 moveDirection = transform.forward * moveVertical;
-                _controller.Move(moveDirection * currentSpeed * Time.deltaTime);
-            }
-            if (Mathf.Abs(moveHorizontal) > 0.01f)
-            {
-                transform.Rotate(Vector3.up, moveHorizontal * rotationSpeed * Time.deltaTime);
-            }
+            animator.SetFloat("MoveSpeed", 0);
         }
     }
 
     private void HandleJump()
     {
         _isGrounded = _controller.isGrounded;
-        if (_isGrounded && Input.GetKeyDown(KeyCode.Space))
+
+        if (_isGrounded && Input.GetButtonDown("Jump"))
         {
             _velocity.y = Mathf.Sqrt(jumpForce * -2f * gravity);
+            animator.SetBool("IsJumping", true);
+
+            // Выбираем анимацию в зависимости от движения
+            if (animator.GetFloat("MoveSpeed") > 0)
+            {
+                animator.Play("JumpOnMove");
+            }
+            else
+            {
+                animator.Play("JumpOnPlace");
+            }
         }
     }
 
     private void ApplyGravity()
     {
-        if (_isGrounded && _velocity.y < 0)
+        bool wasGrounded = _isGrounded;
+        _isGrounded = _controller.isGrounded;
+
+        if (_isGrounded)
         {
+            if (!wasGrounded) // Проверяем, был ли персонаж в воздухе
+            {
+                animator.SetBool("IsJumping", false);
+            }
+
             _velocity.y = -2f;
         }
+
         _velocity.y += gravity * Time.deltaTime;
         _controller.Move(_velocity * Time.deltaTime);
     }
